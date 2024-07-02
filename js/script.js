6b7df3494d16fe24dd82a348417b83539a628178
Rodnext <[leoinfotecrj@gmail.com]>
2024-07-02 20:08:13 -0300

    "Refactored CSS, HTML, and JavaScript code for timer functionality, including layout changes, class updates, and timer logic adjustments."

--- js/script.js
+++ js/script.js
@@ -2,52 +2,68 @@ let idTemporizador = 0;
 let temporizadores = [];
 let timerValues = {};
 
-$('#add-temporizador').on('click', function() {
+$('#add-temporizador').on("click", function () {
   idTemporizador++;
   let html = `
-        <tr class="spacer-row bg-transparent bg-none">
-            <td colspan="7" style="height: 20px;"></td> <!-- Colspan para ocupar todas as colunas -->
-          </tr>
-        <tr>
-          <td class="nome-temp mx-auto col-12">
-            <div class="input-group">
+
+  
+        <div class="col-lg-4 col-sm-6">
+          <div class="card mt-0 mb-3" >
+            <div class="input-group mb-3">
               <input type="text" id="nome-${idTemporizador}" value="" placeholder="Nome da sua vila" class="form-control nome-input">
-              <div class="input-group-append ">
+              <div class="input-group-append">
                 <button class="btn btn-secondary lock" data-id="${idTemporizador}"><i class="ri-lock-unlock-line"></i></button>
               </div>
             </div>
-          </td>
-          <td class="agrupar">
-            <div  class="min-md-th-show text-dark dica-dd">
-              <p class="font-weight-bold m-0"> Dias</p>
+
+
+            <div class="row align-items-center text-dark ">
+              <div class="agrupar col-lg-2 col-sm-6 col-2">
+                  <div class="form-group ">
+                    <p class="font-weight-bold m-1 small-text"> Dias</p>
+                  <input type="number" id="dias-${idTemporizador}" value="0" min="0" placeholder="DD" class="form-control tempo-input larger-input" oninput="validateInput(this, 30)">
+                </div>
+              </div>
+
+              <div class="agrupar  col-lg-2 col-sm-6 col-2">
+                  <div  class="form-group">
+                    <p class="font-weight-bold m-1 small-text"> Hrs</p>
+                  <input type="number" id="horas-${idTemporizador}" value="0" min="0" max="23" placeholder="HH" class="form-control tempo-input larger-input" oninput="validateInput(this, 24)">
+                </div>
+              </div>
+
+              <div class="agrupar  col-lg-2 col-sm-6 col-2">
+                  <div class="form-group">
+                    <p class="font-weight-bold m-1 small-text"> Mins</p>
+                  <input type="number" id="minutos-${idTemporizador}" value="0" min="0" max="59" placeholder="MM" class="form-control tempo-input larger-input" oninput="validateInput(this, 60)">
+                </div>
+              </div>
+              <div class="agrupar  col-lg-2 col-sm-6 col-2">
+                  <div  class="form-group">
+                    <p class="font-weight-bold m-1 small-text"> Segs</p>
+                  <input type="number" id="segundos-${idTemporizador}" value="0" min="0" max="59" placeholder="SS" class="form-control tempo-input larger-input" oninput="validateInput(this, 60)">
+                </div>
+              </div>
+
+                <div class="agrupar-botao btn-group mt-1 col-lg-2 col-md col-sm col-2 justify-content-center" >
+
+                  <div  class="form-group" id="iniciar-${idTemporizador}">
+                    <p class="font-weight-bold mb-1 small-text">Iniciar</p>
+                    <button class="font-weight-bold btn btn-warning btn-lg rounded-circle"><i class="ri-play-fill"></i></button>
+                  </div>
+
+                  <div  class="form-group" id="parar-${idTemporizador}" style="display: none;">
+                      <p class="font-weight-bold mb-1 small-text">Parar</p>
+                    <button class="font-weight-bold btn btn-danger btn-lg rounded-circle"><i class="ri-stop-fill"></i></button>
+                  </div>
+
+                </div>
+
+              </div>
             </div>
-            <input type="number" id="dias-${idTemporizador}" value="0" min="0" placeholder="DD" class="form-control tempo-input" oninput="validateInput(this, 30)">
-          </td>
-          <td  class="agrupar">
-            <div  class="min-md-th-show text-dark dica-dd">
-              <p class="font-weight-bold m-0"> Hrs</p>
-            </div>
-            <input type="number" id="horas-${idTemporizador}" value="0" min="0" max="23" placeholder="HH" class="form-control tempo-input" oninput="validateInput(this, 24)">
-          </td>
-          <td class="agrupar">
-            <div  class="min-md-th-show text-dark dica-dd">
-              <p class="font-weight-bold m-0"> Mins</p>
-            </div>
-            <input type="number" id="minutos-${idTemporizador}" value="0" min="0" max="59" placeholder="MM" class="form-control tempo-input" oninput="validateInput(this, 60)">
-          </td>
-          <td class="agrupar">
-            <div  class="min-md-th-show text-dark dica-dd">
-              <p class="font-weight-bold m-0"> Segs</p>
-            </div>
-            <input type="number" id="segundos-${idTemporizador}" value="0" min="0" max="59" placeholder="SS" class="form-control tempo-input" oninput="validateInput(this, 60)">
-          </td>
-          <td  class="agrupar-botao">
-            <div class="btn-group">
-              <button class="btn btn-warning rounded-circle" id="iniciar-${idTemporizador}"><i class="ri-play-fill"></i></button>
-              <button class="btn btn-danger rounded-circle" id="parar-${idTemporizador}"  style="display: none;"><i class="ri-stop-fill"></i></button>
-            </div>
-          </td>
-        </tr>
+          </div>
+        </div>
+
   `;
   $('#temporizadores').append(html);
 });
@@ -56,64 +72,68 @@ $(document).on('click', '.lock', function() {
   let id = $(this).data('id');
   let nomeInput = $(`#nome-${id}`);
   this.classList.toggle('active');
-  if (nomeInput.prop('readonly')) {
-    nomeInput.prop('readonly', false);
-    $(this).html('<i class="ri-lock-unlock-line"></i>');
-  } else {
-    nomeInput.prop('readonly', true);
-    $(this).html('<i class="ri-lock-line"></i>');
-  }
+  nomeInput.prop('readonly', !nomeInput.prop('readonly'));
+  $(this).find('i').toggleClass('ri-lock-unlock-line ri-lock-line');
 });
 
 function validateInput(input, maxValue) {
   if (input.value > maxValue) {
-      input.value = maxValue;
+    input.value = maxValue;
   }
 }
 
+$(document).on('click', '.tempo-input', function() {
+  $(this).val('');
+  
+$('.tempo-input').on('blur', function() {
+  if ($(this).val() === '') {
+    $(this).val(0);
+  }
+});
+});
+
 $(document).on('click', '[id^="iniciar-"]', function() {
   let id = $(this).attr('id').replace('iniciar-', '');
-  $(`#dias-${id}`).prop('readonly', true);
-  $(`#horas-${id}`).prop('readonly', true);
-  $(`#minutos-${id}`).prop('readonly', true);
-  $(`#segundos-${id}`).prop('readonly', true);
+  $(`#dias-${id}, #horas-${id}, #minutos-${id}, #segundos-${id}`).prop('readonly', true);
 
-  const dias = parseInt($(`#dias-${id}`).val());
-  const horas = parseInt($(`#horas-${id}`).val());
-  const minutos = parseInt($(`#minutos-${id}`).val());
-  const segundos = parseInt($(`#segundos-${id}`).val());
-
-  const endTime = Date.now() + dias * 86400000 + horas * 3600000 + minutos * 60000 + segundos * 1000;
+  timerValues[id] = {
+    dias: parseInt($(`#dias-${id}`).val()),
+    horas: parseInt($(`#horas-${id}`).val()),
+    minutos: parseInt($(`#minutos-${id}`).val()),
+    segundos: parseInt($(`#segundos-${id}`).val())
+  };
 
   temporizadores[id] = setInterval(function() {
-    const now = Date.now();
-    const timeLeft = endTime - now;
-
-    if (timeLeft <= 0) {
-      clearInterval(temporizadores[id]);
-      $(`#dias-${id}`).val(0);
-      $(`#horas-${id}`).val(0);
-      $(`#minutos-${id}`).val(0);
-      $(`#segundos-${id}`).val(0);
-      $(`#iniciar-${id}`).show();
-      $(`#parar-${id}`).hide();
-      return;
+    timerValues[id].segundos--;
+    if (timerValues[id].segundos < 0) {
+      timerValues[id].segundos = 59;
+      timerValues[id].minutos--;
+      if (timerValues[id].minutos < 0) {
+        timerValues[id].minutos = 59;
+        timerValues[id].horas--;
+        if (timerValues[id].horas < 0) {
+          timerValues[id].horas = 23;
+          timerValues[id].dias--;
+          if (timerValues[id].dias < 0) {
+            clearInterval(temporizadores[id]);
+            $(`#dias-${id}, #horas-${id}, #minutos-${id}, #segundos-${id}`).prop('readonly', false).val(0);
+            timerValues[id] = { dias: 0, horas: 0, minutos: 0, segundos: 0 };
+            $(`#iniciar-${id}`).show();
+            $(`#parar-${id}`).hide();
+          }
+        }
+      }
     }
-
-    const days = Math.floor(timeLeft / 86400000);
-    const hours = Math.floor((timeLeft % 86400000) / 3600000);
-    const minutes = Math.floor((timeLeft % 3600000) / 60000);
-    const seconds = Math.floor((timeLeft % 60000) / 1000);
-
-    $(`#dias-${id}`).val(days);
-    $(`#horas-${id}`).val(hours);
-    $(`#minutos-${id}`).val(minutes);
-    $(`#segundos-${id}`).val(seconds);
-
-    $(`#iniciar-${id}`).hide();
-    $(`#parar-${id}`).show();
+    $(`#dias-${id}`).val(timerValues[id].dias);
+    $(`#horas-${id}`).val(timerValues[id].horas);
+    $(`#minutos-${id}`).val(timerValues[id].minutos);
+    $(`#segundos-${id}`).val(timerValues[id].segundos);
   }, 1000);
+  
+  $(this).hide();
+  $(`#parar-${id}`).show();
 });
+
 
 $(document).on('click', '[id^="parar-"]', function() {
   let id = $(this).attr('id').replace('parar-', '');
@@ -132,21 +152,20 @@ $(document).on('click', '[id^="parar-"]', function() {
 
 $('#ordenar-tempo').on('click', function() {
   const temporizadores = [];
-  $('#temporizadores tr').each(function() {
-    const id = $(this).find('td:eq(0) input').attr('id').replace('nome-', '');
+  $('#temporizadores .col-lg-4').each(function() {
+    const id = $(this).find('input[type="text"]').attr('id').replace('nome-', '');
     const dias = parseInt($(`#dias-${id}`).val());
     const horas = parseInt($(`#horas-${id}`).val());
     const minutos = parseInt($(`#minutos-${id}`).val());
     const segundos = parseInt($(`#segundos-${id}`).val());
     const tempoTotal = dias * 86400 + horas * 3600 + minutos * 60 + segundos;
-    temporizadores.push({ id, tempoTotal, tr: $(this) });
+    temporizadores.push({ id, tempoTotal, element: $(this) });
   });
 
   temporizadores.sort((a, b) => a.tempoTotal - b.tempoTotal);
 
-  const tbody = $('#temporizadores');
-  tbody.html('');
+  $('#temporizadores').empty();
   temporizadores.forEach((temporizador) => {
-    tbody.append(temporizador.tr);
+    $('#temporizadores').append(temporizador.element);
   });
 });
