5683ceb9bd635705c243fd0c9d242512c65e90c7
Rodnext <[leoinfotecrj@gmail.com]>
2024-07-17 19:25:41 -0300

    "Refactored HTML, CSS, and JavaScript code for timer functionality, added dark mode toggle, and updated styles."

--- script.js
+++ script.js
@@ -1,14 +1,59 @@
 let idTemporizador = 0;
 let temporizadores = [];
-let timerValues = {};
 
+document.addEventListener("DOMContentLoaded", function() {
+  const sidebar = document.getElementById("sidebar");
+  const sidebarToggle = document.getElementById("sidebarToggle");
+  const overlay = document.getElementById("overlay");
+  const darkModeToggle = document.querySelector('.btn-dark-mode');
 
+  function toggleSidebar() {
+    sidebar.classList.toggle("collapsed");
+  }
+
+  function checkWindowSize() {
+    if (window.innerWidth < 992) {
+      sidebar.classList.add("collapsed");
+    } else {
+      sidebar.classList.remove("collapsed");
+    }
+  }
+
+  sidebarToggle.addEventListener("click", toggleSidebar);
+  window.addEventListener("resize", checkWindowSize);
+
+  // Verifica o tamanho da janela ao carregar a página
+  checkWindowSize();
+
+  darkModeToggle.addEventListener("click", function() {
+    document.body.classList.toggle("dark-mode");
+    document.querySelector('.content').classList.toggle('dark-mode');
+    const icon = darkModeToggle.querySelector("i");
+    const cards = document.querySelectorAll('.card');
+    const sidebars = document.querySelectorAll('.sidebar');
+    cards.forEach(function(card) {
+      card.classList.toggle('dark-mode');
+    });
+    sidebars.forEach(function(sidebar) {
+      sidebar.classList.toggle('dark-mode');
+    });
+    if (document.body.classList.contains("dark-mode")) {
+      icon.classList.remove("ri-moon-line");
+      icon.classList.add("ri-sun-line");
+    } else {
+      icon.classList.remove("ri-sun-line");
+      icon.classList.add("ri-moon-line");
+    }
+  });
+});
+
+$(document).ready(function() {
 $('#add-temporizador').on("click", function () {
   idTemporizador++;
   let html = `
-        <div class="col-xl-3 col-lg-4 col-md-4 col-sm-6 p-1"  id="card-0">
-          <div class="card p-1" >
-            <div class="input-group mt-2">
+        <div class="col-xl-3 col-lg-4 col-md-4 col-sm-6 p-1" id="card-${idTemporizador}"  data-id="${idTemporizador}">
+          <div class="card p-1 pb-3">
+            <div class="input-group mt-1 mb-2 border-bottom">
               <button class="btn border-0 btn-sm mr-1 ml-1 btndelete" id="btndelete-${idTemporizador}"><i class="ri-close-large-line"></i></button>
               <input type="text" id="nome-${idTemporizador}" value="" placeholder="Nome da sua vila" class="font-weight-bold form-control form-control-sm nome-input">
               <div class="input-group-append">
@@ -16,140 +61,177 @@ $('#add-temporizador').on("click", function () {
               </div>
             </div>
             
-            <div class="row align-items-center text-dark mx-auto align-items-center">
-              <div class="agrupar">
-                  <div class="form-group">
-                    <p class="font-weight-bold m-1 small-text"> D</p>
-                  <input type="number" id="dias-${idTemporizador}" value="0" min="0" placeholder="DD" class="form-control tempo-input larger-input" oninput="validateInput(this, 30)">
-                </div>
-              </div>
+          <p class="m-0 p-0">Vila principal</p>
+            ${createTimerHTML(idTemporizador, 1)}
+          <p class="m-0 mt-1  p-0">Laboratório</p>
+            ${createTimerHTML(idTemporizador, 2)}
+          <p class="m-0 mt-1  p-0">Casa de Animais</p>
+            ${createTimerHTML(idTemporizador, 3)}
+          <p class="m-0 mt-1  p-0">Base do Construtor</p>
+            ${createTimerHTML(idTemporizador, 4)}
 
-              <div class="agrupar">
-                  <div  class="form-group">
-                    <p class="font-weight-bold m-1 small-text"> H</p>
-                  <input type="number" id="horas-${idTemporizador}" value="0" min="0" max="23" placeholder="HH" class="form-control tempo-input larger-input" oninput="validateInput(this, 24)">
-                </div>
-              </div>
-
-              <div class="agrupar">
-                  <div class="form-group">
-                    <p class="font-weight-bold m-1 small-text"> M</p>
-                  <input type="number" id="minutos-${idTemporizador}" value="0" min="0" max="59" placeholder="MM" class="form-control tempo-input larger-input" oninput="validateInput(this, 60)">
-                </div>
-              </div>
-              <div class="agrupar">
-                  <div  class="form-group">
-                    <p class="font-weight-bold m-1 small-text"> S</p>
-                  <input type="number" id="segundos-${idTemporizador}" value="0" min="0" max="59" placeholder="SS" class="form-control tempo-input larger-input" oninput="validateInput(this, 60)">
-                </div>
-              </div>
-
-                <div class="agrupar-botao btn-group mt-1 justify-content-center" >
-
-                  <div  class="form-group" id="iniciar-${idTemporizador}">
-                    <p class="font-weight-bold mb-1 small-text">Iniciar</p>
-                    <button class="font-weight-bold btn btn-warning btn-sm rounded-circle"><i class="ri-play-fill"></i></button>
-                  </div>
-
-                  <div class="form-group" id="parar-${idTemporizador}" style="display: none;">
-                    <p class="font-weight-bold mb-1 small-text">Parar</p>
-                    <button class="font-weight-bold btn btn-danger btn-sm rounded-circle"><i class="ri-stop-fill"></i></button>
-                  </div>
-
-                </div>
-              </div>
-            </div>
           </div>
+        </div>
   `;
   $('#temporizadores').append(html);
 });
-//Quando clicado trava o botão de nome da vila
+
+function createTimerHTML(id, timerId) {
+  return `
+    <div class="row text-dark mx-auto justify-content-center align-items-center">
+      <div class="agrupar">
+        <div class="form-group">
+          <input type="number" id="dias-${id}-${timerId}" value="0" min="0" placeholder="DD" class="form-control tempo-input larger-input" oninput="validateInput(this, 30)">
+        </div>
+      </div>
+
+      <div class="agrupar">
+        <div class="form-group">
+          <input type="number" id="horas-${id}-${timerId}" value="0" min="0" max="23" placeholder="HH" class="form-control tempo-input larger-input" oninput="validateInput(this, 24)">
+        </div>
+      </div>
+
+      <div class="agrupar">
+        <div class="form-group">
+          <input type="number" id="minutos-${id}-${timerId}" value="0" min="0" max="59" placeholder="MM" class="form-control tempo-input larger-input" oninput="validateInput(this, 60)">
+        </div>
+      </div>
+
+      <div class="agrupar">
+        <div class="form-group">
+          <input type="number" id="segundos-${id}-${timerId}" value="0" min="0" max="59" placeholder="SS" class="form-control tempo-input larger-input" oninput="validateInput(this, 60)">
+        </div>
+      </div>
+
+      <div class="agrupar-botao btn-group justify-content-center">
+        <div class="form-group" id="iniciar-${id}-${timerId}">
+          <button class="font-weight-bold btn btn-warning btn-sm rounded-circle"><i class="ri-play-fill"></i></button>
+        </div>
+
+        <div class="form-group" id="parar-${id}-${timerId}" style="display: none;">
+          <button class="font-weight-bold btn btn-danger btn-sm rounded-circle"><i class="ri-stop-fill"></i></button>
+        </div>
+      </div>
+    </div>
+  `;
+}
+$('#add-temporizador').trigger("click");
+
+
+  $('.dropdown-item').on('click', function() {
+    let filterId = $(this).data('filter');
+    filterCards(filterId);
+  });
+
+  function filterCards(filterId) {
+    let cardsArray = [];
+
+    $('.card').each(function() {
+      let card = $(this);
+      let cardId = card.closest('.col-xl-3, .col-lg-4, .col-md-4, .col-sm-6').attr('data-id');
+      let dias = parseInt(card.find(`#dias-${cardId}-${filterId}`).val()) || 0;
+      let horas = parseInt(card.find(`#horas-${cardId}-${filterId}`).val()) || 0;
+      let minutos = parseInt(card.find(`#minutos-${cardId}-${filterId}`).val()) || 0;
+      let segundos = parseInt(card.find(`#segundos-${cardId}-${filterId}`).val()) || 0;
+
+      let totalSeconds = (dias * 86400) + (horas * 3600) + (minutos * 60) + segundos;
+
+      cardsArray.push({ card: card.parent(), totalSeconds: totalSeconds });
+    });
+
+    cardsArray.sort((a, b) => a.totalSeconds - b.totalSeconds);
+
+    $('#temporizadores').html('');
+    cardsArray.forEach(item => {
+      $('#temporizadores').append(item.card);
+    });
+  }
+});
+
 $(document).on('click', '.lock', function() {
-  let id = $(this).data('id');
-  let nomeInput = $(`#nome-${id}`);
+  const id = $(this).data('id');
+  const nomeInput = $(`#nome-${id}`);
   this.classList.toggle('active');
   nomeInput.prop('readonly', !nomeInput.prop('readonly'));
   $(this).find('i').toggleClass('ri-lock-unlock-line ri-lock-line');
 });
 
-//Define um valor maximo para cada campo imput
 function validateInput(input, maxValue) {
   if (input.value > maxValue) {
     input.value = maxValue;
   }
 }
 
-/*Quando clicado esconde o valor do imput
-Quando sai do campo sem preencher ele retorna a zero*/
 $(document).on('click', '.tempo-input', function() {
-  $(this).val('s'); 
-    $('.tempo-input').on('blur', function() {
-      if ($(this).val() === '') {
-        $(this).val(0);
-      }
-    });
+  $(this).val('');
+  $('.tempo-input').on('blur', function() {
+    if ($(this).val() === '') {
+      $(this).val(0);
+    }
+  });
 });
 
-
-/*Quando clicado no botão de iniciar inicia o temporizador
-o botão iniciar é ocultado e aparece o botão parar*/
 $(document).on('click', '[id^="iniciar-"]', function() {
-  let id = $(this).attr('id').replace('iniciar-', '');
-  $(`#dias-${id}, #horas-${id}, #minutos-${id}, #segundos-${id}`).prop('readonly', true);
+  let ids = $(this).attr('id').split('-');
+  let id = ids[1];
+  let timerId = ids[2];
 
-  const dias = parseInt($(`#dias-${id}`).val());
-  const horas = parseInt($(`#horas-${id}`).val());
-  const minutos = parseInt($(`#minutos-${id}`).val());
-  const segundos = parseInt($(`#segundos-${id}`).val());
+  $(`#dias-${id}-${timerId}, #horas-${id}-${timerId}, #minutos-${id}-${timerId}, #segundos-${id}-${timerId}`).prop('readonly', true);
+
+  const dias = parseInt($(`#dias-${id}-${timerId}`).val());
+  const horas = parseInt($(`#horas-${id}-${timerId}`).val());
+  const minutos = parseInt($(`#minutos-${id}-${timerId}`).val());
+  const segundos = parseInt($(`#segundos-${id}-${timerId}`).val());
 
   const totalTimeInSeconds = dias * 86400 + horas * 3600 + minutos * 60 + segundos;
   const startTime = Date.now();
 
-  temporizadores[id] = setInterval(function() {
+  temporizadores[`${id}-${timerId}`] = setInterval(function() {
     const elapsedTimeInSeconds = Math.floor((Date.now() - startTime) / 1000);
     const remainingTimeInSeconds = totalTimeInSeconds - elapsedTimeInSeconds;
 
     if (remainingTimeInSeconds <= 0) {
-      clearInterval(temporizadores[id]);
-      $(`#dias-${id}, #horas-${id}, #minutos-${id}, #segundos-${id}`).prop('readonly', false).val(0);
-      $(`#iniciar-${id}`).show();
-      $(`#parar-${id}`).hide();
+      clearInterval(temporizadores[`${id}-${timerId}`]);
+      $(`#dias-${id}-${timerId}, #horas-${id}-${timerId}, #minutos-${id}-${timerId}, #segundos-${id}-${timerId}`).prop('readonly', false).val(0);
+      $(`#iniciar-${id}-${timerId}`).show();
+      $(`#parar-${id}-${timerId}`).hide();
     } else {
       const remainingDias = Math.floor(remainingTimeInSeconds / 86400);
       const remainingHrs = Math.floor((remainingTimeInSeconds % 86400) / 3600);
       const remainingMins = Math.floor((remainingTimeInSeconds % 3600) / 60);
       const remainingSecs = remainingTimeInSeconds % 60;
 
-      $(`#dias-${id}`).val(remainingDias);
-      $(`#horas-${id}`).val(remainingHrs);
-      $(`#minutos-${id}`).val(remainingMins);
-      $(`#segundos-${id}`).val(remainingSecs);
+      $(`#dias-${id}-${timerId}`).val(remainingDias);
+      $(`#horas-${id}-${timerId}`).val(remainingHrs);
+      $(`#minutos-${id}-${timerId}`).val(remainingMins);
+      $(`#segundos-${id}-${timerId}`).val(remainingSecs);
     }
   }, 1000);
 
   $(this).hide();
-  $(`#parar-${id}`).show();
+  $(`#parar-${id}-${timerId}`).show();
 });
 
-/*Quando clicado em parar interrompe o temporizador, zera os campos inputs e
-esconde o botão de parar e libera todos os campos*/
 $(document).on('click', '[id^="parar-"]', function() {
-  let id = $(this).attr('id').replace('parar-', '');
-  clearInterval(temporizadores[id]);
-  $(`#dias-${id}, #horas-${id}, #minutos-${id}, #segundos-${id}`).prop('readonly', false);
-  $(`#dias-${id}`).val(0);
-  $(`#horas-${id}`).val(0);
-  $(`#minutos-${id}`).val(0);
-  $(`#segundos-${id}`).val(0);
-  $(`#iniciar-${id}`).show();
-  $(`#parar-${id}`).hide();
+  let ids = $(this).attr('id').split('-');
+  let id = ids[1];
+  let timerId = ids[2];
+
+  clearInterval(temporizadores[`${id}-${timerId}`]);
+  $(`#dias-${id}-${timerId}, #horas-${id}-${timerId}, #minutos-${id}-${timerId}, #segundos-${id}-${timerId}`).prop('readonly', false);
+  $(`#dias-${id}-${timerId}`).val(0);
+  $(`#horas-${id}-${timerId}`).val(0);
+  $(`#minutos-${id}-${timerId}`).val(0);
+  $(`#segundos-${id}-${timerId}`).val(0);
+  $(`#iniciar-${id}-${timerId}`).show();
+  $(`#parar-${id}-${timerId}`).hide();
 });
+
 
 $(document).on('click', '.btndelete', function() {
   $(this).closest('.col-xl-3').remove();
 });
-
-//Ordena os cards por ordem de menor tempo no temporizador de cada um
+/*
 $('#ordenar-tempo').on('click', function() {
   const temporizadores = [];
   $('#temporizadores .col-lg-4').each(function() {
@@ -169,50 +251,4 @@ $('#ordenar-tempo').on('click', function() {
     $('#temporizadores').append(temporizador.element);
   });
 });
-
-document.addEventListener("DOMContentLoaded", function() {
-  var sidebar = document.getElementById("sidebar");
-  var sidebarToggle = document.getElementById("sidebarToggle");
-  var overlay = document.getElementById("overlay");
-
-  function toggleSidebar() {
-      sidebar.classList.toggle("collapsed");
-  }
-
-  function checkWindowSize() {
-      if (window.innerWidth < 992) {
-          sidebar.classList.add("collapsed");
-      } else {
-          sidebar.classList.remove("collapsed");
-      }
-  }
-
-  sidebarToggle.addEventListener("click", toggleSidebar);
-  window.addEventListener("resize", checkWindowSize);
-
-  // Verifica o tamanho da janela ao carregar a página
-  checkWindowSize();
-
-
-  
-  darkModeToggle.addEventListener("click", function() {
-    document.body.classList.toggle("dark-mode");
-    document.querySelector('.content').classList.toggle('dark-mode');
-    var icon = darkModeToggle.querySelector("i");
-    var cards = document.querySelectorAll('.card');
-    var sidebar = document.querySelectorAll('.sidebar');
-      cards.forEach(function(card) {
-        card.classList.toggle('dark-mode');
-    });
-      sidebar.forEach(function(sidebar) {
-        sidebar.classList.toggle('dark-mode');
-    });
-    if (document.body.classList.contains("dark-mode")) {
-      icon.classList.remove("ri-moon-line");
-      icon.classList.add("ri-sun-line");
-    } else {
-      icon.classList.remove("ri-sun-line");
-      icon.classList.add("ri-moon-line");
-    }
-  });
-});
+*/
